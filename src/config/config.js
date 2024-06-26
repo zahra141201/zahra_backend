82d585ed64b6f8109c437626ea23a216c8203cb0
Zahra Colas <zahracolas@Ordinateur-portable-de-Zahra.local>
2024-06-26 14:53:50 -0400

    configuration des DB dans config

---
+++ src/config/config.js
@@ -0,0 +1,30 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+module.exports = {
+  "development": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": `${process.env.DB_NAME}_development`,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres",
+    seederStorage: "sequelize",
+  },
+  "test": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": `${process.env.DB_NAME}_test`,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres",
+    seederStorage: "sequelize",
+  },
+  "production": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": `${process.env.DB_NAME}_production`,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres",
+    seederStorage: "sequelize",
+  }
+};
