95633530f5cee5cb5a0bc9bcd76046c15d34292b
Zahra Colas <zahracolas@Ordinateur-portable-de-Zahra.local>
2024-06-27 11:02:22 -0400

    revisions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 // src/index.js
 const Koa = require('koa');
 const Logger = require('koa-logger');
-const cors = require('@koa/cors'); // Importer le middleware CORS
+const cors = require('@koa/cors');
 const router = require('./routes'); // Importer le routeur principal
 
 const app = new Koa();
 
 app.use(Logger());
 app.use(cors()); // Utiliser le middleware CORS
+
+// Middleware pour logger toutes les requêtes
+app.use(async (ctx, next) => {
+  console.log(`Received ${ctx.method} request for ${ctx.url}`);
+  await next();
+});
 
 app.use(router.routes()); // Utiliser le routeur principal
 
