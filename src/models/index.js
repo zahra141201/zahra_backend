eab0294c4b836c337a0d42d61755466a3a931b26
Zahra Colas <zahracolas@Ordinateur-portable-de-Zahra.local>
2024-06-27 12:32:14 -0400

    revisions

--- src/models/index.js
+++ src/models/index.js
@@ -1,29 +1,34 @@
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
+const dotenv = require("dotenv");
 const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const path = require('path');
+const fs = require('fs');
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
+
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
+fs.readdirSync(__dirname)
   .filter(file => {
     return (
       file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
+      file !== 'index.js' &&
+      file.slice(-3) === '.js'
     );
   })
   .forEach(file => {
