9e24532d6a3567b42f2bd91184b7cf840e715a39
Zahra Colas <zahracolas@Ordinateur-portable-de-Zahra.local>
2024-06-26 23:06:52 -0400

    revision

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,42 +1,101 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      this.hasMany(models.Ingredient, {
-        foreignKey: 'owner'
-      });
-      this.hasMany(models.Request, {
-        foreignKey: 'made_by'
-      });
-      this.hasMany(models.Valoration, {
-        foreignKey: 'email_user'
-      });
-      this.hasMany(models.ShoppingCart, {
-        foreignKey: 'email_cliente'
-      })
+const Router = require('koa-router');
+var jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
+
+dotenv.config();
+
+
+const router = new Router();
+
+router.post("authentication.signup", "/signup", async(ctx) => {
+    const authInfo = ctx.request.body;
+    let user = await ctx.orm.User.findOne({ where: { email: authInfo.email } })
+    if (user) {
+        ctx.body = `The user by email '${authInfo.email}' already exists`;
+        ctx.status = 400;
+        return;
     }
-  }
-  User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    name: DataTypes.STRING,
-    telephone: DataTypes.INTEGER,
-    member_since: DataTypes.DATE,
-    address: DataTypes.STRING,
-    description: DataTypes.STRING,
-    is_admin: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
-};
+    try {
+        const saltRounds = 10;
+        const hashPassword = await bcrypt.hash(authInfo.password, saltRounds);
+        user = await ctx.orm.User.create({
+            email: authInfo.email,
+            password: hashPassword,
+            name: authInfo.name,
+            telephone: authInfo.telephone,
+            member_since: authInfo.member_since,
+            address: authInfo.address,
+            description: authInfo.description,
+            is_admin: authInfo.is_admin
+        })
+    } catch (error) {
+        ctx.body = error;
+        ctx.status = 400;
+        return;
+    }
+    ctx.body = {
+        email: user.email,
+        name: user.name
+    };
+    ctx.status = 201;
+})
+
+
+
+router.post("authentication.login", "/login", async(ctx) => {
+    let user;
+    const authInfo = ctx.request.body;
+    try {
+        user = await ctx.orm.User.findOne({ where: { email: authInfo.email } })
+    }
+    
+    catch(error) {
+        ctx.body = error;
+        ctx.status = 400;
+        return;
+    }
+
+    if (!user) {
+        ctx.body = `The user by email '${authInfo.email}' was not found`;
+        ctx.status = 400;
+        return;
+    }
+    const validPassword = await bcrypt.compare(authInfo.password, user.password);
+
+    if (validPassword) {
+        ctx.body = {
+            email: user.email,
+            name: user.name
+        };
+        ctx.status = 200;
+
+    } else {
+        ctx.body = "Incorrect password";
+        ctx.status = 400;
+        return;
+    }
+
+    /// Creamos el JWT. Si quisieras agregar distintos scopes, como por ejemplo
+    /// "admin", podrían hacer un llamado a la base de datos y cambiar el payload
+    /// en base a eso
+
+    const expirationSeconds = 1 * 60 * 60 * 24;
+    const JWT_PRIVATE_KEY = process.env.JWT_SECRET;
+    
+
+    var token = jwt.sign(
+        { scope: ['user'] },
+        JWT_PRIVATE_KEY,
+        { subject: user.id.toString() },
+        { expiresIn: expirationSeconds }
+    );
+
+    ctx.body = {
+        "access_token": token,
+        "token_type": "Bearer",
+        "expires_in": expirationSeconds
+    };
+    ctx.status = 200;
+})
+module.exports = router
